460f182247027634af29fc3c8d6684eb03eb138b
repositoriocm <repositoriocm@citi.com.mx>
2019-02-05 07:27:06 -0600

    siguiente actualizacion

--- SVB/Model/Deck.cs
+++ SVB/Model/Deck.cs
@@ -12,17 +12,17 @@ namespace SVB.Core.Model
 	public class Deck
 	{
 		public string name { get; set; }
-		public List<Card> cards { get; set; }
-		public leaderClass leaderClass { get; set; }
-		public Card[] draw(int Q, cardType _cardtype = cardType.Any, bool _isCountdown = false,
-			trait hasTrait = trait.NoTrait, leaderClass _class = leaderClass.All, ability hasAbility = ability.None,
-			int? MaxPpCost = null)
-		{
-			var paramSearch = new List<Card>();
-			if ((_cardtype == cardType.Amulet))
-			{
-				paramSearch = cards.Cast<Amulet>().Select(a => a.signature == _cardtype && a.isCountdown == _isCountdown);
-			}
-		}
-	}
-}
+ 		public List<Card> cards { get; set; }
+ 		public leaderClass leaderClass { get; set; }
+ 		public Card[] draw(int Q, cardType _cardtype = cardType.Any, bool _isCountdown = false,
+ 			trait hasTrait = trait.NoTrait, leaderClass _class = leaderClass.All, ability hasAbility = ability.None,
+ 			int? MaxPpCost = null)
+ 		{
+ 			var paramSearch = new List<Card>();
+ 			if ((_cardtype == cardType.Amulet))
+ 			{
+ 				paramSearch = cards.Cast<Amulet>().Select(a => a.signature == _cardtype && a.isCountdown == _isCountdown);
+ 			}
+ 		}
+ 	}
+ }
